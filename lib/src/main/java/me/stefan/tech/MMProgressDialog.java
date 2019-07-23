99dcd188598daf8b6824426167c28bc8f5a68525
zsfenggy <zsfenggy@hotmail.com>
2019-07-23 16:06:28 +0800

    小改动

--- lib/src/main/java/me/stefan/tech/MMProgressDialog.java
+++ lib/src/main/java/me/stefan/tech/MMProgressDialog.java
@@ -10,9 +10,10 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import me.stefan.tech.R;
 
 public class MMProgressDialog extends Dialog {
 
